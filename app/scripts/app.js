0aa61b20abe4be806773605ffca86ea1d7a7cd1b
Malibu <matthieu-p93@live.fr>
2015-02-25 02:38:23 +0100

    update

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,63 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name museumGamesApp
- * @description
- * # museumGamesApp
- *
- * Main module of the application.
- */
-angular
-  .module('museumGamesApp', [
+(function() {
+
+
+  var monApp = angular.module('museumGamesApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ui.router'
   ]);
+
+  monApp.config(["$stateProvider", "$urlRouterProvider", "$httpProvider",
+   function($stateProvider, $urlRouterProvider, $httpProvider) {
+
+    //$httpProvider.defaults.headers.post = {'Content-Type' : "text/plain"};
+
+    $stateProvider
+      .state('index', {
+        url: "/referents",
+        templateUrl: "views/home.html",
+        controller: 'MainCtrl'
+      })
+
+    .state('referentGames', {
+      url: "/referents/{idReferent}/games",
+      templateUrl: "views/referent_games.html",
+      controller: 'MainCtrl'
+    })
+
+    .state('game', {
+      url: "/referents/{idReferent}/games/{idGame}",
+      templateUrl: "views/play.html",
+      controller: 'MainCtrl'
+    })
+
+
+    .state('login', {
+      url: "/login",
+      templateUrl: "views/auth/login.html",
+      controller: 'MainCtrl'
+    })
+    .state('referent', {
+      url: "/referent",
+      templateUrl: "views/referent.html",
+      controller: 'MainCtrl'
+    })
+    .state('admin', {
+      url: "/admin",
+      templateUrl: "views/admin.html",
+      controller: 'MainCtrl'
+    })
+
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/referents");
+
+  }]);
+
+
+
+})();
